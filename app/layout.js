1d06f7d90ed0f1d15120a56b1de07a6062bef9be
zandermonroezm-sudo <zander.monroe.zm@gmail.com>
2026-09-24 10:07:41 -0500

    Add app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"Fantasy Command Center",description:"Roster-aware ESPN fantasy football decision center",manifest:"/manifest.webmanifest"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
